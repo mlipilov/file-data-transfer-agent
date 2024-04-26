435c3aece91fc19af81f66c7d2b05fe58355bff2
Mykhailo Lipilov <m.lipilov@andersenlab.com>
2024-04-26 19:48:21 +0300

    AND-1 Initial commit

---
+++ src/main/java/com/andersen/filedatatransferagent/FileDataTransferAgentApplication.java
@@ -0,0 +1,12 @@
+package com.andersen.filedatatransferagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDataTransferAgentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FileDataTransferAgentApplication.class, args);
+  }
+}
---
+++ src/test/java/com/andersen/filedatatransferagent/FileDataTransferAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersen.filedatatransferagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDataTransferAgentApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
