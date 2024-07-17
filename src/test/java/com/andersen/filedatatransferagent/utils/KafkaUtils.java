c052922c91022651763c292a26327c08c9ec58f0
Mykhailo Lipilov <m.lipilov@andersenlab.com>
2024-07-17 15:34:52 +0300

    AND-8 Started fixing comments from the last assessment attempt#13

--- src/test/java/com/andersen/filedatatransferagent/utils/KafkaUtils.java
+++ src/test/java/com/andersen/filedatatransferagent/utils/KafkaUtils.java
@@ -5,9 +5,18 @@ import lombok.experimental.UtilityClass;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.ssl.DefaultSslBundleRegistry;
 
+/**
+ * This class provides utility methods for working with Apache Kafka.
+ */
 @UtilityClass
 public class KafkaUtils {
 
+  /**
+   * Retrieves consumer properties for Kafka based on the provided KafkaProperties object.
+   *
+   * @param kafkaProperties The KafkaProperties object containing the configuration for Kafka.
+   * @return The consumer properties for the Kafka consumer.
+   */
   public static Properties getConsumerProperties(final KafkaProperties kafkaProperties) {
     final var sslBundles = new DefaultSslBundleRegistry();
     final var consumerProps = kafkaProperties.buildConsumerProperties(sslBundles);
