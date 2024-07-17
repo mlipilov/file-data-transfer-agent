440696ee282635dc70c38baec76359048b6b8064
Mykhailo Lipilov <m.lipilov@andersenlab.com>
2024-07-17 15:09:10 +0300

    AND-8 Started fixing comments from the last assessment attempt#9

--- src/main/java/com/andersen/filedatatransferagent/controller/UserCsvDataTransferApi.java
+++ src/main/java/com/andersen/filedatatransferagent/controller/UserCsvDataTransferApi.java
@@ -1,6 +1,11 @@
 package com.andersen.filedatatransferagent.controller;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import com.andersen.filedatatransferagent.exception.model.ErrorRs;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
@@ -13,9 +18,21 @@ public interface UserCsvDataTransferApi {
       description = "Used to perform user data migration",
       operationId = "transferUserCsvData")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Successful operation"),
-      @ApiResponse(responseCode = "400", description = "Invalid request"),
-      @ApiResponse(responseCode = "500", description = "Internal server error")
+      @ApiResponse(
+          responseCode = "200",
+          description = "Successful operation"),
+      @ApiResponse(
+          responseCode = "400",
+          description = "Invalid request",
+          content = @Content(
+              mediaType = APPLICATION_JSON_VALUE,
+              schema = @Schema(implementation = ErrorRs.class))),
+      @ApiResponse(
+          responseCode = "500",
+          description = "Internal server error",
+          content = @Content(
+              mediaType = APPLICATION_JSON_VALUE,
+              schema = @Schema(implementation = ErrorRs.class)))
   })
   ResponseEntity<Void> transferUserCsvData(final MultipartFile csvData);
 }
