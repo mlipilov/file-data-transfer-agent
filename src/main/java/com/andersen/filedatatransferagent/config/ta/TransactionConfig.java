99317ad6fa64a1fad7c4fbf115eacdf36224cbf2
Mykhailo Lipilov <m.lipilov@andersenlab.com>
2024-07-21 12:35:27 +0300

    AND-8 Started fixing comments from the last assessment attempt#20

--- src/main/java/com/andersen/filedatatransferagent/config/ta/TransactionConfig.java
+++ src/main/java/com/andersen/filedatatransferagent/config/ta/TransactionConfig.java
@@ -7,11 +7,20 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.support.JdbcTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
+/**
+ * This class is a configuration class for managing transactions in the application.
+ * It provides a bean definition for the transaction manager using the given data source.
+ */
 @Configuration
 public class TransactionConfig {
 
-  @Primary
+  /**
+   * This method creates and configures a transaction manager for managing transactions in the application.
+   *
+   * @param dataSource the data source used for the transaction manager
+   * @return the transaction manager for*/
   @Bean
+  @Primary
   public PlatformTransactionManager transactionManager(
       final DataSource dataSource
   ) {
